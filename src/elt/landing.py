fca17c438a480511290d5898bafb4d33fe49a2b0
Konrad van Kempen <konradg@live.nl>
2025-11-19 08:58:03 +0100

    change old loading to landing

--- src/elt/landing.py
+++ src/elt/landing.py
@@ -5,13 +5,16 @@ import json
 import logging
 import os
 from datetime import datetime
+from db_helper import get_db_connection
+
+
 
 logger = logging.getLogger(__name__)
 
 MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
-MINIO_BUCKET = os.getenv("MINIO_BUCKET", "spotify-data")
+MINIO_BUCKET = os.getenv("MINIO_BUCKET")
 
 
 class StorageContext:
@@ -19,7 +22,6 @@ class StorageContext:
         self.client = Minio(
             endpoint, access_key=access_key, secret_key=secret_key, secure=False
         )
-
 
 def create_bucket(context: StorageContext) -> None:
     try:
@@ -31,7 +33,6 @@ def create_bucket(context: StorageContext) -> None:
     except S3Error as e:
         logger.error(f"Error creating bucket {MINIO_BUCKET}: {e}")
         raise
-
 
 def load_data_to_bucket(context: StorageContext, object_name: str, data: dict) -> None:
     try:
@@ -52,6 +53,43 @@ def load_data_to_bucket(context: StorageContext, object_name: str, data: dict) -
             create_bucket(context)
             load_data_to_bucket(context, object_name, data)
 
+def init_log_landing_loading():
+    #Create the log_landing_loading table if it doesn't exist
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS public.log_landing_loading (
+                id SERIAL PRIMARY KEY,
+                object_name VARCHAR(255) UNIQUE NOT NULL,
+                bucket_name VARCHAR(255) NOT NULL,
+                landed_at TIMESTAMP NOT NULL,
+                loaded_at TIMESTAMP,
+                status VARCHAR(50) NOT NULL,
+            );
+        """)
+        logger.info("Tracking table initialized successfully")
+    
+def insert_log_landing_loading(object_name, timestamp):
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO public.log_landing_loading
+            (object_name, bucket_name, landed_at, loaded_at, status)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (
+            object_name,
+            MINIO_BUCKET,
+            timestamp,
+            None,
+            "landed"
+        ))
+
+def update_log_landed(object_name: str, timestamp_loaded: datetime) -> None:
+    """Update PostgreSQL log table when data lands in MinIO"""
+
+    init_log_landing_loading()
+
+    insert_log_landing_loading(object_name, timestamp_loaded)
 
 def handler(data: dict):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -65,3 +103,9 @@ def handler(data: dict):
 
     load_data_to_bucket(ctx, object_name, data)
     logger.info("Data load process completed successfully")
+
+    """ Updates the log so that later can be checken when landing happened
+    , but loading didn't """
+    update_log_landed(object_name, timestamp)
+    
+    return object_name
