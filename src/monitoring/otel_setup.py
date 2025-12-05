c4092608d9e429bd6a3c31338f71b0a3d5a43bce
Konrad van Kempen <konradg@live.nl>
2025-12-05 14:27:02 +0100

    updated otel to send to loki

--- src/monitoring/otel_setup.py
+++ src/monitoring/otel_setup.py
@@ -1,25 +1,38 @@
 import logging
 
 from opentelemetry._logs import set_logger_provider
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor, ConsoleLogExporter
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
 
+
 # otel_setup.py
-def setup_otel_logging(level=logging.DEBUG):
+def setup_otel_logging(
+    otel_collector_host: str,
+    otel_collector_port: int,
+    insecure: bool,
+    level=logging.DEBUG,
+):
     logger_provider = LoggerProvider(
-        resource=Resource.create({
-            "service.name": "goats-elt",
-            "service.instance.id": "goats-elt-instance",
-        }),
+        resource=Resource.create(
+            {
+                "service.name": "goats-elt",
+                "service.instance.id": "goats-elt-instance",
+            }
+        ),
     )
     set_logger_provider(logger_provider)
-    
-    exporter = ConsoleLogExporter()
+
+    # Configure OTLP exporter to send logs to the collector
+    exporter = OTLPLogExporter(
+        endpoint=f"{otel_collector_host}:{otel_collector_port}", insecure=insecure
+    )
+
     logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
-    
+
     handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
-    
+
     root_logger = logging.getLogger()
     root_logger.setLevel(level)
     root_logger.addHandler(handler)
