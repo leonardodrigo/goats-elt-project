039cd695868fafb72fb5beee6966f90e3073b9c6
roudis <alkisroudis@gmail.com>
2025-12-05 13:42:25 +0100

    refactor: standardize formatting in main.tf for modules and outputs

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -39,54 +39,54 @@ module "db" {
 }
 
 module "minio" {
-  source              = "./minio"
-  network_name        = module.network.network_name
-  minio_root_user     = var.minio_root_user
-  minio_root_password = var.minio_root_password
+  source               = "./minio"
+  network_name         = module.network.network_name
+  minio_root_user      = var.minio_root_user
+  minio_root_password  = var.minio_root_password
   minio_default_bucket = var.minio_default_bucket
 }
 
 module "streamlit" {
-  source                = "./streamlit"
-  depends_on            = [module.app_api]
-  project_root_path     = var.project_root_path
-  network_name          = module.network.network_name
-  db_user               = var.db_user
-  db_pass               = var.db_pass
-  db_name               = var.db_name
-  db_port               = var.db_port
-  db_host               = var.db_host
+  source            = "./streamlit"
+  depends_on        = [module.app_api]
+  project_root_path = var.project_root_path
+  network_name      = module.network.network_name
+  db_user           = var.db_user
+  db_pass           = var.db_pass
+  db_name           = var.db_name
+  db_port           = var.db_port
+  db_host           = var.db_host
 }
 
 module "app_api" {
-  source                = "./app_api"
-  depends_on            = [module.minio, module.db, module.kafka]
-  project_root_path     = var.project_root_path
-  network_name          = module.network.network_name
-  db_host               = var.db_host
-  db_port               = var.db_port
-  db_user               = var.db_user
-  db_pass               = var.db_pass
-  db_name               = var.db_name
-  dbt_dataset           = var.dbt_dataset
-  dbt_dev_name          = var.dbt_dev_name
-  minio_endpoint        = var.minio_endpoint
-  minio_root_user       = var.minio_root_user
-  minio_root_password   = var.minio_root_password
-  minio_default_bucket  = var.minio_default_bucket
-  spotify_client_id     = var.spotify_client_id
-  spotify_client_secret = var.spotify_client_secret
-  spotify_redirect_uri  = var.spotify_redirect_uri
-  spotify_refresh_token = var.spotify_refresh_token
-  spotify_scopes        = var.spotify_scopes
+  source                  = "./app_api"
+  depends_on              = [module.minio, module.db, module.kafka]
+  project_root_path       = var.project_root_path
+  network_name            = module.network.network_name
+  db_host                 = var.db_host
+  db_port                 = var.db_port
+  db_user                 = var.db_user
+  db_pass                 = var.db_pass
+  db_name                 = var.db_name
+  dbt_dataset             = var.dbt_dataset
+  dbt_dev_name            = var.dbt_dev_name
+  minio_endpoint          = var.minio_endpoint
+  minio_root_user         = var.minio_root_user
+  minio_root_password     = var.minio_root_password
+  minio_default_bucket    = var.minio_default_bucket
+  spotify_client_id       = var.spotify_client_id
+  spotify_client_secret   = var.spotify_client_secret
+  spotify_redirect_uri    = var.spotify_redirect_uri
+  spotify_refresh_token   = var.spotify_refresh_token
+  spotify_scopes          = var.spotify_scopes
   kafka_bootstrap_servers = var.kafka_bootstrap_servers
-  kafka_topic            = var.kafka_topic
-  poll_interval          = var.poll_interval
+  kafka_topic             = var.kafka_topic
+  poll_interval           = var.poll_interval
 }
 
 module "kafka" {
-  source                = "./kafka"
-  network_name          = module.network.network_name
+  source       = "./kafka"
+  network_name = module.network.network_name
 }
 
 output "spotify_redirect_uri" {
